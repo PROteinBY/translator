2b05da9f2409d043dcfe39412ff36c64e61f8464
Siarhei Blashuk <siarhei_blashuk@epam.com>
2017-12-04 16:18:14 +0300

    client refactoring

--- client/assets/js/controller.js
+++ client/assets/js/controller.js
@@ -7,17 +7,20 @@ const DOMEN = "localhost";
 const URL = "http://" + DOMEN + ":8080/translate";
 
 function translationController(data) {
-    if (data.result === ERROR) {
-        $("#support-area").show();
-        $("#word").val(data.word);
-        $("#message").val(data.reason);
-    }
-    else if (data.result === OK) {
-        $("#output").val(data.text)
-    }
-    else {
-        console.log(data)
-    }
+    console.log("Error controller: " + JSON.stringify(data));
+
+    $("#output").val(data.text)
+}
+
+function errorController(data) {
+    console.log("Error controller: " + data.responseText);
+
+    var reason = data.responseJSON.reason;
+    var word = data.responseJSON.word;
+
+    $("#support-area").show();
+    $("#word").val(word);
+    $("#message").val(reason);
 }
 
 function send() {
@@ -25,78 +28,27 @@ function send() {
         type:"POST",
         url: URL,
         contentType: "application/json",
-        dataType: "json",
-        data: {
+        data: JSON.stringify({
             text: text,
             dict: words
-        },
-        success: translationController
+        }),
+        success: translationController,
+        error:errorController
     });
 }
 
 $(document).ready(function(){
+    $("#support-area").hide();
+
     $("#translate").click(function() {
         words = {};
         text = $("#input").val();
+        console.log("Text: " + text);
         send()
     });
 
-    $("send-translation").click(function() {
+    $("#send-translation").click(function() {
         words[$("#word").val()] = $("#translation").val();
         send()
     });
 });
-
-
-
-/*
-CLIENT ->
-{
-    "text": "привет боб это петух",
-    "dict": {
-
-    }
-}
-
-SERVER ->
-
-{
-    "result": "error",
-    "reason": "нет перевода слова боб",
-    "word": "боб"
-}
-
-CLIENT ->
-
-{
-    "text": "привет боб это петух галимый",
-    "dict": {
-        "боб": "bob"
-    }
-}
-
-SERVER ->
-
-{
-    "result": "error",
-    "reason": "нет перевода слова петух",
-    "word": "петух"
-}
-
-CLIENT ->
-
-{
-    "text": "привет боб это петух",
-    "dict": {
-        "боб": "bob",
-        "петух": "cock"
-    }
-}
-
-SERVER ->
-
-{
-    "result": "ok",
-    "text": "hi bob it's cock"
-}
- */
